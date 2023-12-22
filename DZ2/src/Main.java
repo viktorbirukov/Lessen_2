336a82c3cd93125d8d38bfa9df0c8c6501782ee7
viktorbirukov <154085669+viktorbirukov@users.noreply.github.com>
2023-12-22 18:11:50 +0200

    Add files via upload

---
+++ DZ2/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello MINSK!");
+        int number = 3;
+        System.out.println(number);
+        int nomer = 4;
+        int numb = 6;
+        int numberr = 3;
+        System.out.println(nomer + numb - numberr);
+
+    }
+}
